1e45d474a162e470447ce7048c476191141db17d
potter bl <zeexample42@gmail.com>
2023-04-24 14:10:47 +0300

    fix userName

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Container from "./Container";
 import '../styles/UserInfo.css'
+import axios from "axios";
 const UserInfo = (props) => {
-    const userName = '{ ' + props.fullName + ' }'
+    const [fullName, setFullName] = useState('')
+    const token = localStorage.getItem('token')
+    useEffect(() => {
+        axios
+            .post(`https://task-euo4.onrender.com/auth/${token}`)
+            .then((res) => {
+                setFullName(res.data.fullName)
+            })
+    }, [token, setFullName])
+    const userName = '{ ' + fullName + ' }'
     const userAvatar = 'https://th.bing.com/th/id/OIP.vBovwYsKrGLBskjH1UKNiwAAAA?pid=ImgDet&rs=1'
     return (
         <div className={'userInfo'}>
