a95f89785ed5a3e9857dcd926d8f3244dbc322d9
potter bl <zeexample42@gmail.com>
2023-05-01 23:53:15 +0300

    update demo-ver

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React from 'react';
+// import React, {useState} from 'react';
 import '../styles/Header.css'
 // import hamburgerMenuWhite from '../images/menuWhite.svg'
 // import hamburgerMenuBlack from '../images/menuBlack.svg'
